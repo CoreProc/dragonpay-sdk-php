4664b8031780da8c1720651c8b47e305484af488
Justin de Leon <jusmdeleon@gmail.com>
2015-04-06 16:23:08 +0800

    Add validation on DragonpayClient merchant credentials

--- src/DragonpayClient.php
+++ src/DragonpayClient.php
@@ -2,6 +2,7 @@
 
 namespace Coreproc\Dragonpay;
 
+use Coreproc\Dragonpay\Exceptions\ValidationException;
 use Exception;
 use Katzgrau\KLogger\Logger;
 
@@ -9,17 +10,17 @@ class DragonpayClient
 {
 
     /**
-     * @var string Merchant's ID
+     * @var string Merchant ID
      */
     private $merchantId;
 
     /**
-     * @var string Merchant's Password
+     * @var string Merchant Password
      */
     private $merchantPassword;
 
     /**
-     * @var bool Enable or disable logging
+     * @var bool Enable/disable logging
      */
     private $logging = false;
 
@@ -29,12 +30,17 @@ class DragonpayClient
     private $logger;
 
     /**
-     * @param array $credentials
+     * @param array $credentials Merchant Credentials
      * @param bool $logging
      * @param null|string $logDirectory
+     * @throws ValidationException
      */
     public function __construct(array $credentials, $logging = false, $logDirectory = null)
     {
+        if (empty($credentials['merchantId']) || empty($credentials['merchantPassword'])) {
+            throw new ValidationException('Please set the Merchant ID or password.');
+        };
+
         $this->merchantId = $credentials['merchantId'];
         $this->merchantPassword = $credentials['merchantPassword'];
         $this->setLogger($logging, $logDirectory);
@@ -57,23 +63,6 @@ class DragonpayClient
     }
 
     /**
-     * Set the logger if logging is set to true.
-     *
-     * @param bool $logging
-     * @param string $logDirectory
-     * @throws Exception
-     */
-    private function setLogger($logging, $logDirectory)
-    {
-        if ($logging == true && ! is_dir($logDirectory)) {
-            throw new Exception('Please set a valid directory in order to enable logging.');
-        }
-
-        $this->logging = true;
-        $this->logger = new Logger($logDirectory);
-    }
-
-    /**
      * Check if logging is enabled.
      *
      * @return bool
@@ -90,6 +79,23 @@ class DragonpayClient
     public function getLogger()
     {
         return $this->logger;
+    }
+
+    /**
+     * Set the logger if logging is set to true.
+     *
+     * @param bool $logging
+     * @param string $logDirectory
+     * @throws Exception
+     */
+    private function setLogger($logging, $logDirectory)
+    {
+        if ($logging == true && ! is_dir($logDirectory)) {
+            throw new Exception('Please set a valid directory in order to enable logging.');
+        }
+
+        $this->logging = true;
+        $this->logger = new Logger($logDirectory);
     }
 
 }
