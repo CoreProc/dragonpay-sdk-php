853477beba16a910722728886c4e0ace7f5bab67
Justin de Leon <jusmdeleon@gmail.com>
2015-05-04 16:31:58 +0800

    Update examples

--- postback.php
+++ postback.php
@@ -1,3 +1,28 @@
 <?php
 
-var_dump($_GET);exit;
+require 'vendor/autoload.php';
+require 'examples/Client.php';
+
+use Coreproc\Dragonpay\Transaction;
+
+$transaction = new Transaction($client);
+
+$params = [
+    'transactionId' => $_GET['txnid'],
+    'referenceNumber' => $_GET['refno'],
+    'status' => $_GET['status'],
+    'message' => $_GET['message'],
+    'digest' => $_GET['digest'],
+
+];
+
+$result = $transaction->isSuccessful($params);
+
+if ($result == true) {
+    // Perform necessary db updates/whatever back-end process required
+} else {
+
+}
+
+var_dump($result);
+
--- return.php
+++ return.php
@@ -1,11 +1,9 @@
 <?php
 
-
 require 'vendor/autoload.php';
 require 'examples/Client.php';
 
 use Coreproc\Dragonpay\Transaction;
-
 
 $transaction = new Transaction($client);
 
@@ -17,4 +15,16 @@ $params = [
 	'digest' => $_GET['digest'],
 	
 ];
-var_dump($transaction->isSuccessful($params));
+
+// Result of transaction checked using the query string params (like on postback).
+// If a Postback URL is used and DB updates were made accordingly,
+// the checking can be made directly on the database.
+$result = $transaction->isSuccessful($params);
+
+if ($result == true) {
+    // Perform necessary db updates/whatever back-end process required
+} else {
+
+}
+
+var_dump($result);
