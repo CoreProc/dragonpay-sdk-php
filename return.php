ddf3c00359d8540e70d31e4086a80e3b33549795
Justin de Leon <jusmdeleon@gmail.com>
2015-05-04 15:26:23 +0800

    Update return example

--- return.php
+++ return.php
@@ -1,3 +1,10 @@
 <?php
 
-var_dump($_GET);exit;
+require 'vendor/autoload.php';
+require 'examples/Client.php';
+
+use GuzzleHttp\Transaction;
+
+$transaction = new \Coreproc\Dragonpay\Transaction($client);
+
+var_dump($transaction->isSuccessful($_GET));
