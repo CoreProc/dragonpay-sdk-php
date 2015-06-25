03382d8556a6ab4887419f67b89af785ec6eb7d2
Justin de Leon <jusmdeleon@gmail.com>
2015-06-25 16:04:21 +0800

    Remove examples

--- examples/TransactionCancellation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Coreproc\Dragonpay\Transaction;
-
-require 'Client.php';
-
-$transaction = new Transaction($client);
-
-var_dump($transaction->cancel('11245678901234567899'));
--- examples/TransactionInquiry.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Coreproc\Dragonpay\Transaction;
-
-require 'Client.php';
-
-$transaction = new Transaction($client);
-
-var_dump($transaction->inquire('mockPending2'));
